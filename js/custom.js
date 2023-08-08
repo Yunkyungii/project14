acfbbdfff0b9718a1f1937e35b6329709586699b
yunkyung <133613667+Yunkyungii@users.noreply.github.com>
2023-08-08 18:18:17 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -11,8 +11,48 @@ $(function () {
 
     const CENTERSLIDE = new Swiper('.center_slide', {
         loop: true,
-        slidesPerView: 1.5,
+        //slidesPerView: 1.3,
+        spaceBetween: 100,
         centeredSlides: true,
-        spaceBetween: 80,
+        slidesPerView: "auto",
+        navigation: {
+            nextEl: "#main_center_info .arrows .right",
+            prevEl: "#main_center_info .arrows .left",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
     });
-})
+
+    $('.center_tit li').on('click', function (e) {
+        e.preventDefault();
+        const idx = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on')
+        CENTERSLIDE.slideTo(idx + 2);
+    })
+
+
+    const TVSLIDE = new Swiper('.tv_slide', {
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+    });
+
+    const HEALTHSLIDE = new Swiper('.health_slide', {
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+    });
+
+    $('.tab_menu .tab li').on('click', function (e) {
+        e.preventDefault();
+        let idx = $(this).index();
+        $('.tab_area>*').eq(idx).addClass('on').siblings().removeClass('on');
+        $(this).addClass('on').siblings().removeClass('on');
+    });
+});
+
