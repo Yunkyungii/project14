367b990a26b9def292730a6ad1229b1d61075927
yunkyung <133613667+Yunkyungii@users.noreply.github.com>
2023-09-20 15:14:19 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -32,7 +32,17 @@ $(function () {
         },
     });
 
+    const SERVICESLIDE = new Swiper('.service_slide', {
+        // loop: true,
+        slidesPerView: 8,
+        navigation: {
+            nextEl: ".service_arrows .right",
+            prevEl: ".service_arrows .left",
+        },
+    })
+
     const CENTERSLIDE = new Swiper('.center_slide', {
+
         loop: true,
         //slidesPerView: 1.3,
         spaceBetween: 100,
@@ -45,53 +55,105 @@ $(function () {
         pagination: {
             el: ".swiper-pagination",
         },
+        on: {
+            slideChangeTransitionStart: function () {
+                // console.log(idx, this.activeIndex)
+                $('.center_tit li')
+                    .eq(this.activeIndex)
+                    .addClass('on')
+                    .siblings()
+                    .removeClass('on')
+            }
+        }
     });
 
     $('.center_tit li').on('click', function (e) {
         e.preventDefault();
         const idx = $(this).index();
+        // console.log(idx, CENTERSLIDE.realIndex);
         $(this).addClass('on').siblings().removeClass('on')
-        CENTERSLIDE.slideTo(idx + 2);
+        CENTERSLIDE.slideTo(idx);
     })
 
+    const TEXTSLIDE = new Swiper('.text_slide', {
+        // loop: true,
+    });
+
+    CENTERSLIDE.controller.control = TEXTSLIDE;
+    TEXTSLIDE.controller.control = CENTERSLIDE;
+
+
+
+    const RESLIDE = new Swiper('.research_slide', {
+        slidesPerView: 3,
+        spaceBetween: 20,
+    })
+
+    const HEALTHSLIDE = new Swiper('.health_slide', {
+        slidesPerView: 1,
+    });
+
+
+    // 탭메뉴 - main board
+    $('#main_board .notice_tab li').on('click', function (e) {
+        e.preventDefault();
+        let idx = $(this).index();
+        $('#main_board .content .swiper').eq(idx).addClass('on').siblings().removeClass('on');
+        $(this).addClass('on').siblings().removeClass('on');
+    });
+
+    const SCHSLIDE = new Swiper('.schedule_slide', {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: "#main_board .arrows .right",
+            prevEl: "#main_board .arrows .left",
+        },
+    });
+    const SRYSLIDE = new Swiper('.story_slide', {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: "#main_board .arrows .right",
+            prevEl: "#main_board .arrows .left",
+        },
+    });
+    const NOTICESLIDE = new Swiper('.notice_slide', {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: "#main_board .arrows .right",
+            prevEl: "#main_board .arrows .left",
+        },
+    });
+    const NEWSSLIDE = new Swiper('.news_slide', {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: "#main_board .arrows .right",
+            prevEl: "#main_board .arrows .left",
+        },
+    });
 
     const TVSLIDE = new Swiper('.tv_slide', {
         loop: true,
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: ".health_tv .arrows .right",
+            prevEl: ".health_tv  .arrows .left",
         },
-        slidesPerView: 2,
-        spaceBetween: 16,
-    });
-
-    const HEALTHSLIDE = new Swiper('.health_slide', {
-        loop: true,
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
-        slidesPerView: 3,
-        spaceBetween: 8,
-    });
-
-    $('.tab_menu .tab li').on('click', function (e) {
-        e.preventDefault();
-        let idx = $(this).index();
-        $('.tab_area>*').eq(idx).addClass('on').siblings().removeClass('on');
-        $(this).addClass('on').siblings().removeClass('on');
     });
 
 
     $(window).on('resize', function () {
         $('#header .gnb').removeClass('on');
-        $('.sub').reomoveArrt('style');
+        // $('.sub').reomoveArrt('style');
     })
 
-$('#header .gnb button').on('click', function(e){
-    e.preventDefault();
-    $('#header .gnb').toggleClass('on')
-});
+    $('#header .gnb button').on('click', function (e) {
+        e.preventDefault();
+        $('#header .gnb').toggleClass('on')
+    });
 
     $('#header .gnb .dth1>li>a').on('click', function (e) {
         if ($('#header .gnb .dth1').hasClass('on')) {
@@ -100,7 +162,6 @@ $('#header .gnb button').on('click', function(e){
             $(this).toggleClass('on');
             // $(this).parent().siblings().find('.sub').slideUp();
         }
-
     });
 
 
